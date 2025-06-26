cd8cc0e965a9e50eecb18f1d05d3b82b75c8d368
Kah Keng Wong <48384560+kahkengwong@users.noreply.github.com>
2025-06-26 19:18:28 +0800

    Update and rename Part-3.7-DE-vs-nonDE-scatter-plot.r to Part-3.8-DE-vs-nonDE-scatter-plot.r

--- Part-3.8-DE-vs-nonDE-scatter-plot.r
+++ Part-3.8-DE-vs-nonDE-scatter-plot.r
@@ -1,5 +1,5 @@
 # ====================================================================
-# 3.7 Dev_explained (purple) vs non_dev_explained (gray) Scatter Plot
+# 3.8 Dev_explained (purple) vs non_dev_explained (gray) Scatter Plot
 # ====================================================================
 library(dplyr)
 library(tibble)
