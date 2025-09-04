c2cdf127e5411a322799656d48d32ff4c93e8a3b
Kah Keng Wong <48384560+kahkengwong@users.noreply.github.com>
2025-09-04 16:46:20 +0800

    Update and rename Part_3.08_Visualize_TPRS_&_GAM_Components.r to Part_3.09_Visualize_TPRS_&_GAM_Components.r

--- Part_3.09_Visualize_TPRS_&_GAM_Components.r
+++ Part_3.09_Visualize_TPRS_&_GAM_Components.r
@@ -1,5 +1,5 @@
 #############################################################
-# Part 3.08: Visualization of TPRS and GAM Components 
+# Part 3.09: Visualization of TPRS and GAM Components 
 #############################################################
 
 # =========================================
@@ -868,5 +868,6 @@ print(examples_viz$dot_plot)
 cat(examples_viz$explanation)
 # Prints the summary table of component values.
 print(examples_viz$summary_table)
+
 
 
