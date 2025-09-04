89721dd9853e3a6882f9c547ba6297d6b42fd1f8
Kah Keng Wong <48384560+kahkengwong@users.noreply.github.com>
2025-09-04 16:25:58 +0800

    Update and rename Part_3.03_GAM_REML_PRSS_Analysis.r to Part_3.04_GAM_REML_PRSS_Analysis.r

--- Part_3.04_GAM_REML_PRSS_Analysis.r
+++ Part_3.04_GAM_REML_PRSS_Analysis.r
@@ -1,5 +1,5 @@
 ###########################################################
-# Part 3.03: GAM-REML-PRSS Analysis and Results Export
+# Part 3.04: GAM-REML-PRSS Analysis and Results Export
 ###########################################################
 # Analyze all gene sets for a given sample
 analyze_multiple_gene_sets <- function(integrated_obj, original_obj, cluster_ids, gene_sets, sample) {
@@ -963,3 +963,4 @@ process_file(pca_file)
 process_file(nonca_file)
 
 cat("All files have been updated with Is_Best_Model column in the REML_Summary sheets.\n")
+
