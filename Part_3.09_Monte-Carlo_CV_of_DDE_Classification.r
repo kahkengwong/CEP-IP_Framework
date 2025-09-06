c26dfdcb2949b200a4a11bd655a7558ad70f1e4c
Kah Keng Wong <48384560+kahkengwong@users.noreply.github.com>
2025-09-06 19:13:17 +0800

    Update Part_3.09_Monte-Carlo_CV_of_DDE_Classification.r

--- Part_3.09_Monte-Carlo_CV_of_DDE_Classification.r
+++ Part_3.09_Monte-Carlo_CV_of_DDE_Classification.r
@@ -1,6 +1,6 @@
 # =================================================================================================================
-# Part 3.09: Monte-Carlo Cross-Validation (CV) Analysis of Decomposition of Deviance Explained (DDE) Classification
-# Part I: DDE Classification Monte-Carlo CV Analysis with Random Negative Control Classification 
+# Part 3.09: Monte-Carlo Cross-Validation (MCCV) Analysis of Decomposition of Deviance Explained (DDE) Classification
+# Part I: DDE Classification MCCV Analysis with Random Negative Control Classification 
 # =================================================================================================================
 # Cross-validation function for deviance cells
 validate_deviance_cells <- function(current_sample, train_prop = 0.7, n_iterations = 20) {
@@ -2676,4 +2676,5 @@ for (sample_name in tumor_samples) {
 }
 
 cat("\nCompleted generating scatter plots for all samples.\n")
+
 
